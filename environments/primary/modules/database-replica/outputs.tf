2db95d7ed0b17a9c3a7ba117d0fe625e32ed6282
Aman Dharpure <amandharpure6@gmail.com>
2026-07-12 18:13:29 +0530

    Remove duplicate environment module folders

--- environments/primary/modules/database-replica/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "replica_identifier" {
-  description = "DR RDS replica identifier"
-  value       = aws_db_instance.replica.identifier
-}
-
-output "replica_endpoint" {
-  description = "DR RDS replica endpoint"
-  value       = aws_db_instance.replica.endpoint
-}
